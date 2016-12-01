0cd9e297a3c5322b00194eb7523a32dc77f19361
Arun <arunsingh.49@gmail.com>
2016-12-01 15:02:38 +0530

    first commit

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 
-// Test2 catch 404 and forward to error handler
+// Test7 catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
